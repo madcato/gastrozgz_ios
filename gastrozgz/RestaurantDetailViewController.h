3a96da8e44fa9bd0d32dfe3bc97b0592b61d678f
madcato <dani_vela@me.com>
2013-06-27 11:55:11 +0200

    added sync status

---
+++ gastrozgz/RestaurantDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantDetailViewController.h
+//  gastrozgz
+//
+//  Created by Daniel Vela on 6/27/13.
+//  Copyright (c) 2013 Daniel Vela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantDetailViewController : UIViewController
+
+@end
