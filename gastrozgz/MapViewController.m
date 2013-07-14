39e25bd4ed4f4c91d3da6755bb45c28ebb0dd537
madcato <dani_vela@me.com>
2013-07-14 23:28:20 +0200

    added zoom level help

--- gastrozgz/MapViewController.m
+++ gastrozgz/MapViewController.m
@@ -10,6 +10,9 @@
 
 @interface MapViewController ()
 
+@property (nonatomic, strong) UIView* loadingView;
+@property (nonatomic, strong) NSMutableArray* array;
+
 @end
 
 @implementation MapViewController
@@ -33,7 +36,7 @@
                                          withExtension:@"plist"];
     NSDictionary* tempDict = [NSDictionary dictionaryWithContentsOfURL:url];
     NSArray* tempLocations = [tempDict valueForKey:@"stations"];
-    NSMutableArray* array = [NSMutableArray array];
+    self.array = [NSMutableArray array];
     for (NSDictionary* location in tempLocations) {
         MKPointAnnotation* annotation = [[MKPointAnnotation alloc] init];
         CLLocationCoordinate2D cord;
@@ -43,10 +46,10 @@
         annotation.title = @"Hola";
         annotation.subtitle = @"Adios";
         
-        [array addObject:annotation];
+        [self.array addObject:annotation];
     }
     
-    [self.mapView addAnnotations:array];
+    [self.mapView addAnnotations:self.array];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -86,10 +89,53 @@
     return view;
 }
 
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
+	// Si el zoom es poco, eliminamos las anotaciones y ponemos una vista gris translucida
+	// para que el usuario haga zoom
+	MKCoordinateRegion region = mapView.region;
+	if(region.span.latitudeDelta > 0.02) {
+		[self.mapView removeAnnotations:self.array];
+		[self showHelpView];
+	} else {
+        [mapView addAnnotations:self.array];
+        [self hideHelpView];
+	}
+}
+
 - (void)mapView:(MKMapView *)mapView
  annotationView:(MKAnnotationView *)view
 calloutAccessoryControlTapped:(UIControl *)control {
     
+}
+
+#pragma mark - Help View
+
+- (void)showHelpView
+{
+    if (self.loadingView == nil)
+    {
+        self.loadingView = [[UIView alloc] initWithFrame:CGRectMake(10.0, 160.0, 300.0, 60.0)];
+        self.loadingView.opaque = NO;
+        self.loadingView.backgroundColor = [UIColor darkGrayColor];
+        self.loadingView.alpha = 0.8;
+		self.loadingView.layer.cornerRadius = 15.0;
+		
+		UILabel* label = [[UILabel alloc]initWithFrame:CGRectMake(30.0,0,240.0,60.0)];
+		label.textAlignment = UITextAlignmentCenter;
+		label.numberOfLines = 2;
+		label.font = [UIFont boldSystemFontOfSize:17];
+		label.text = @"Para ver la información, aumente el nivel de zoom.";
+		label.textColor = [UIColor whiteColor];
+		label.backgroundColor = [UIColor clearColor];
+		[self.loadingView addSubview:label];
+    }
+	
+    [self.view addSubview:self.loadingView];
+}
+
+- (void)hideHelpView
+{
+    [self.loadingView removeFromSuperview];
 }
 
 @end
